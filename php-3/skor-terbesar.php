3459193d28d0a6e828436afdb0d1d281bf56604c
Basri <58840135+basri1425@users.noreply.github.com>
2020-06-25 08:21:36 +0700

    update

--- php-3/skor-terbesar.php
+++ php-3/skor-terbesar.php
@@ -1,41 +1,53 @@
 <?php
-
-//Dummy data
-$skor = [
-    ['nama'=>'A', 'kelas'=>'Laravel', 'nilai'=>90],
-    ['nama'=>'B', 'kelas'=>'React Native', 'nilai'=>80],
-    ['nama'=>'C', 'kelas'=>'React JS', 'nilai'=>70],
-    ['nama'=>'D', 'kelas'=>'React Native', 'nilai'=>100],
-    ['nama'=>'E', 'kelas'=>'Laravel', 'nilai'=>50],
-    ['nama'=>'F', 'kelas'=>'React Native', 'nilai'=>90],
-    ['nama'=>'G', 'kelas'=>'Laravel', 'nilai'=>60],
-    ['nama'=>'H', 'kelas'=>'React JS', 'nilai'=>80],
-    ['nama'=>'I', 'kelas'=>'React Native', 'nilai'=>100],
-    ['nama'=>'J', 'kelas'=>'Laravel', 'nilai'=>70],
-    ['nama'=>'K', 'kelas'=>'React JS', 'nilai'=>60],
-];
+// SOAL
+// buatlah sebuah file dengan nama skor-terbesar.php. Di dalam file tersebut buatlah sebuah function dengan nama skor_terbesar yang menerima parameter berupa array berisi array asosiatif data penilaian peserta bootcamp. function akan me-return sebuah array asosiatif yang berisi siswa dengan nilai tertinggi di masing-masing kelas.
 
 function skor_terbesar($arr){
-//kode di sini
-    echo '<pre>';
-    $arr_result = [];
-    foreach ($arr as $row) {
-        # code...
-        if(array_key_exists($row['kelas'], $arr_result)){
-            if($row['nilai']> $arr_result[$row['kelas']]['nilai']){
-                $arr_result[$row['kelas']] = $row;
-            }
-        }else{
-            $arr_result[$row['kelas']] = $row;
-        }
-    }
+  //kode di sini
+  $hasil = [];
+  $nilai = count($arr);
+  //echo $nilai;
+  for($a=0; $a<$nilai; $a++){
+    if($arr[$a]["nilai"] > 84){
+        $hasil[$a] = array("nama" => $arr[$a]['nama'], "kelas" => $arr[$a]['kelas'], "nilai"=> $arr[$a]['nilai']);
+        // print_r($arr)
 
-    print_r($arr_result);
-    echo '<pre>';
+    }
+  }
+  return $hasil;
 }
 
 // TEST CASES
+$skor = [
+  [
+    "nama" => "Bobby",
+    "kelas" => "Laravel",
+    "nilai" => 78
+  ],
+  [
+    "nama" => "Regi",
+    "kelas" => "React Native",
+    "nilai" => 86
+  ],
+  [
+    "nama" => "Aghnat",
+    "kelas" => "Laravel",
+    "nilai" => 90
+  ],
+  [
+    "nama" => "Indra",
+    "kelas" => "React JS",
+    "nilai" => 85
+  ],
+  [
+    "nama" => "Yoga",
+    "kelas" => "React Native",
+    "nilai" => 77
+  ],
+];
+echo '<pre>';
 print_r(skor_terbesar($skor));
+echo '</pre>';
 /* OUTPUT
   Array (
     [Laravel] => Array
